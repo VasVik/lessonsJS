697e5020770d4e2dc2c7ae0a67915e641b89a2cc
Vasiliy <r4801720@gmail.com>
2022-06-21 20:03:31 +0300

    Math object

--- js/script.js
+++ js/script.js
@@ -1,41 +1,5 @@
-let str = '1,2,3,4,5,6,7';
+let num = 2.4;
 
-let arr = str.split(',');
-
-// console.log(arr.join(';'))
-// console.log(arr.reverse())
-// arr.splice(1, 0 , 11, 22)
-// console.log(arr)
-
-let nArr = arr.concat([11,22]);
-// console.log(nArr)
-
-let numArr = nArr.map(function (i) {
-    // return parseInt(i)
-    return i * 2
-});
-
-console.log(numArr);
-
-
-let objArr = [
-    {name: 'Max', age: 27},
-    {name: 'Elena', age: 18},
-    {name: 'Viktor', age: 20},
-    {name: 'Sveta', age: 20}
-];
-
-// return only first object with age 20
-let foundPerson = objArr.find(function (person) {
-    // console.log(person)
-    return person.age === 20
-});
-
-// console.log(foundPerson)
-
-
-let oddArray = [1,2,3,4,5,6,7,7].filter(function (i) {
-    return i % 2 !== 0
-});
-
-console.log(oddArray);
+console.log(Math.random());
+console.log(Math.floor(num)); //окркугление в меньшую сторону
+console.log(Math.ceil(num)); //округлуние в большую сторону

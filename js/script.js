8ee1145c773c85969f6b72f0c035dd4b05ae5a60
Vasiliy <r4801720@gmail.com>
2022-06-22 00:20:29 +0300

    JSON object

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-let num = 2.4;
+let person = {
+    name: 'max',
+    age: '39',
+    car: {
+        model: 'ford'
+    },
+    job: 'admin',
+    friends: ['Elena', 'Igor']
+};
 
-console.log(Math.random());
-console.log(Math.floor(num)); //окркугление в меньшую сторону
-console.log(Math.ceil(num)); //округлуние в большую сторону
+// преобразование обьекта в строку
+let str = JSON.stringify(person);
+
+console.log(str);
+
+// преобразование обратно в объект JSON
+console.log(JSON.parse(str));
+
+console.log(person.car.model);
+console.log(person.friends);

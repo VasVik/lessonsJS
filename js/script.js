82757eec1d8566f2c552bb2f691520e40c0e4b4d
Vasiliy <r4801720@gmail.com>
2022-07-02 05:49:19 +0300

    Timer Implementation

--- js/script.js
+++ js/script.js
@@ -1,18 +1,47 @@
 let $start = document.querySelector('#start');
 let $game = document.querySelector('#game');
+let $timeGame = document.querySelector('#time');
+
+let tGameNum = parseFloat($timeGame.textContent);
 let score = 0;
+let isGameStarted = true;
 
 $start.addEventListener('click', startGame);
 $game.addEventListener('click', handleBoxClick);
 
+
+
 function startGame() {
     $game.style.backgroundColor = '#fff';
     $start.classList.add('hide');
-    
+
+    let intervalId = setInterval(function () {
+
+        if (tGameNum <= 0) {
+            clearInterval(intervalId);
+            endGame();
+        } else {
+            $timeGame.textContent = tGameNum.toFixed(1);
+            tGameNum -= 0.1;
+        }
+
+    }, 100);
+
     renderBox();
 }
 
+function endGame() {
+    // $game.removeEventListener('click', handleBoxClick); //myVersion
+
+    // wfm version
+    isGameStarted = false;
+}
+
 function handleBoxClick(event) {
+
+    if (!isGameStarted) {
+        return;
+    }
 
     if (event.target.dataset.box) {
         score++;
@@ -27,8 +56,8 @@ function renderBox() {
     let gameSize = $game.getBoundingClientRect();
     let maxHW = gameSize.width;
 
-    let boxSize = getRandom(5, maxHW); // myVersion
-    // let boxSize = getRandom(30, 100); // wfmVersion
+    // let boxSize = getRandom(5, maxHW); // myVersion
+    let boxSize = getRandom(30, 100); // wfmVersion
 
     let maxTop = maxHW - boxSize;
     let maxLeft = maxHW - boxSize;

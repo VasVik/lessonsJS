b4e68217384a2c8f7cc21ec39e2b2db6a530d4cd
Vasiliy <r4801720@gmail.com>
2022-07-11 12:20:53 +0300

    Lesson 3. Creating and configuring objects

--- js/script.js
+++ js/script.js
@@ -1,20 +1,53 @@
-function Car(name, year){
-    this.name = name;
-    this.year = year;
+let ford = Object.create({
+    calculateDistancePerYear: function () {
+        Object.defineProperty(this, 'distancePerYear', {
+            // enumerable: true,
+            enumerable: false,
+            writable: false,
+            value: Math.ceil(this.distance / this.age)
+        })
+    }
+},{
+    name: {
+        value: 'Ford',
+        enumerable: true,
+        writable: false,
+        configurable: false
+    },
+    model: {
+        value: 'Focus',
+        enumerable: true,
+        writable: false,
+        configurable: false
+    },
+    year: {
+        value: 2015,
+        enumerable: true,
+        writable: false,
+        configurable: false
+    },
+    distance: {
+        value: 120500,
+        enumerable: true,
+        writable: true,
+        configurable: false
+    },
+    age: {
+        enumerable: true,
+        get: function() {
+            console.log('Получаем возраст');
+            return new Date().getFullYear() - this.year;
+        },
+        set: function() {
+            console.log('Задаем возраст');
+        }
+    }
+});
+
+ford.calculateDistancePerYear();
+
+for (let key in ford) {
+    if (ford.hasOwnProperty(key)){
+        console.log(key, ford[key]);
+    }
 }
-
-Car.prototype.getAge = function () {
-    return new Date().getFullYear() - this.year;
-};
-
-Car.prototype.color = 'black';
-
-let ford = new Car('Ford', 2015);
-let bmw = new Car('BMW', 2017);
-
-ford.color = 'red';
-
-console.log(ford);
-console.log(bmw);
-
-console.log(ford.getAge());

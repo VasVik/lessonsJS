1d1a26a9f621c1105cdfe91477d887ffaa0b6ff9
Vasiliy <r4801720@gmail.com>
2022-07-11 09:53:52 +0300

    Lesson 2. How Prototypes Work

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function Car(name, year){
+    this.name = name;
+    this.year = year;
+}
+
+Car.prototype.getAge = function () {
+    return new Date().getFullYear() - this.year;
+};
+
+Car.prototype.color = 'black';
+
+let ford = new Car('Ford', 2015);
+let bmw = new Car('BMW', 2017);
+
+ford.color = 'red';
+
+console.log(ford);
+console.log(bmw);
+
+console.log(ford.getAge());

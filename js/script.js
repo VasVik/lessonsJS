a45f4a0ce356911cad97973ca07f9c472e082028
Vasiliy <r4801720@gmail.com>
2022-07-12 11:37:32 +0300

    Lesson 4. Iteration of object keys

--- js/script.js
+++ js/script.js
@@ -1,53 +1,28 @@
-let ford = Object.create({
-    calculateDistancePerYear: function () {
-        Object.defineProperty(this, 'distancePerYear', {
-            // enumerable: true,
-            enumerable: false,
-            writable: false,
-            value: Math.ceil(this.distance / this.age)
-        })
-    }
-},{
-    name: {
-        value: 'Ford',
-        enumerable: true,
-        writable: false,
-        configurable: false
-    },
-    model: {
-        value: 'Focus',
-        enumerable: true,
-        writable: false,
-        configurable: false
-    },
-    year: {
-        value: 2015,
-        enumerable: true,
-        writable: false,
-        configurable: false
-    },
-    distance: {
-        value: 120500,
-        enumerable: true,
-        writable: true,
-        configurable: false
-    },
-    age: {
-        enumerable: true,
-        get: function() {
-            console.log('Получаем возраст');
-            return new Date().getFullYear() - this.year;
-        },
-        set: function() {
-            console.log('Задаем возраст');
-        }
-    }
+let person = {
+    name: 'max',
+    age: 39,
+    job: 'admin'
+};
+
+console.log(person);
+
+// итерация ключей обьекта вариант 1
+// проходит в том числе по прототипу
+// дабы исключить ключи в прототипе используется
+// hasOwnProperty
+
+// for (let key in person) {
+//     if (person.hasOwnProperty(key)){
+//         console.log(key, person[key]);
+//     }
+// }
+
+// итерация ключей обьекта вариант 2
+// не идет по прототипу
+
+// let keys = Object.keys(person);
+// console.log(keys);
+
+Object.keys(person).forEach(function (key) {
+    console.log(person[key]);
 });
-
-ford.calculateDistancePerYear();
-
-for (let key in ford) {
-    if (ford.hasOwnProperty(key)){
-        console.log(key, ford[key]);
-    }
-}

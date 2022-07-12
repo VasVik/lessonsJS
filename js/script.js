7faf3447c5215b55142bbc993152ca50297d79cc
Vasiliy <r4801720@gmail.com>
2022-07-12 12:31:33 +0300

    Lesson 5. How closures work

--- js/script.js
+++ js/script.js
@@ -1,28 +1,29 @@
-let person = {
-    name: 'max',
-    age: 39,
-    job: 'admin'
+let createCounter = function (counterName) {
+    let counter = 0;
+
+    // return function () {
+    //     console.log(counterName, ++counter)
+    // }
+
+    return {
+        increment: function () {
+            counter++;
+        },
+        decriment: function () {
+            counter--;
+        },
+        getCounter: function () {
+            return counter;
+        }
+    }
 };
 
-console.log(person);
+let counterA = createCounter('counterA');
+let counterB = createCounter('counterB');
 
-// итерация ключей обьекта вариант 1
-// проходит в том числе по прототипу
-// дабы исключить ключи в прототипе используется
-// hasOwnProperty
+counterA.increment();
+counterA.increment();
+counterA.increment();
 
-// for (let key in person) {
-//     if (person.hasOwnProperty(key)){
-//         console.log(key, person[key]);
-//     }
-// }
-
-// итерация ключей обьекта вариант 2
-// не идет по прототипу
-
-// let keys = Object.keys(person);
-// console.log(keys);
-
-Object.keys(person).forEach(function (key) {
-    console.log(person[key]);
-});
+counterB.decriment();
+counterB.decriment();

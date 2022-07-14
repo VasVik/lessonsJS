46bc2df7577eaa8907fdba5ab966aaee850c1702
Vasiliy <r4801720@gmail.com>
2022-07-14 16:13:41 +0300

    Lesson 1. Variables Let Const

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// for (var i = 0; i < 5; i++ ) {
+//     console.log(i);
+//     setTimeout(function () {
+//         console.log(i);
+//     }, 2000);
+// };
+//
+// for (let i = 0; i < 5; i++ ) {
+//     console.log(i);
+//     setTimeout(function () {
+//         console.log(i);
+//     }, 2000);
+// };
+
+const HEX = '#FFAABB';
+
+const array = [1, 2];
+const obj = {a: 1};
+
+array.unshift(4);
+obj.b = 2;
+
+console.log(array);
+console.log(obj);
+
+document.querySelector('h1').style.color = HEX;

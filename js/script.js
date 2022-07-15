56b4be27448571624e8dea03f4bbb5c7bdb82802
Vasiliy <r4801720@gmail.com>
2022-07-15 18:30:10 +0300

    Lesson 3. Default Options

--- js/script.js
+++ js/script.js
@@ -1,62 +1,20 @@
-// function calculateAge(year) {
-//     const curent = new Date().getFullYear();
-//     return curent - year;
-// }
-//
-// const getAge = year => {
-//     const curent = new Date().getFullYear();
-//     return curent - year;
-// };
-
-// const getAge = year => {
-//     return new Date().getFullYear() - year;
-// };
-
-// const getAge = year => new Date().getFullYear() - year;
-//
-// const logAge = year => console.log(new Date().getFullYear() - year);
-//
-// logAge(1988);
-//
-// console.log(getAge(1987));
-
-// старая реализация
-// const person = {
-//     firstName: 'max',
-//     age: 27,
-//     logNameWithTimeout: function () {
-//         window.setTimeout(function () {
-//             console.log(this.firstName);
-//         }.bind(this),1000);
+// исходый код
+// const createPost = (title, text, date) => {
+//     return {
+//         title: title,
+//         text: text,
+//         date: date
 //     }
 // };
 
-/* новый вариант реализации
-* function создает новый контекст с this,
-* стрелочная функция не создает новый и по этому
-* this используется от контента созданного function(т.е. от person)
-* */
-// const person = {
-//     firstName: 'max',
-//     age: 27,
-//     logNameWithTimeout: function () {
-//         window.setTimeout( () => {
-//             console.log(this.firstName);
-//         },1000)
-//     }
-// };
-
-// 2-й новый вариант реализации
-// равносилен предидущему варианту
-const person = {
-    firstName: 'max',
-    age: 27,
-    logNameWithTimeout() {
-        window.setTimeout(() => {
-            console.log(this.firstName);
-        }, 1000)
-    }
+//упрощенный код + добавлены параметры по умолчанию
+const createPost = (title, text = 'Default text', date = new Date().toLocaleDateString()) => {
+    return {title, text, date}
 };
 
 
-person.logNameWithTimeout();
+const post = createPost('Заголовок', 'Очень важный текст', '15.07.2022');
+console.log(post);
+
+const post2 = createPost('Заголовок 2');
+console.log(post2);

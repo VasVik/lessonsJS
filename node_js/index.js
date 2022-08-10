020a361dccaef11bd59608b95970f346ef2074a9
Vasiliy <r4801720@gmail.com>
2022-08-10 22:04:10 +0300

    Lesson 2. NodeJS Basics

---
+++ node_js/index.js
@@ -0,0 +1,18 @@
+// const now = new Date().toLocaleString();
+// console.log(now);
+
+// console.log(Math.random())
+
+const fs = require('fs');
+const data = `
+    Hello from NodeJS
+    I'm random text!
+`;
+
+fs.writeFileSync('nodejs.txt', data)
+
+const result = fs.readFileSync('nodejs.txt', {encoding: 'utf-8'});
+console.log(result);
+
+console.log(__dirname);
+console.log(__filename);

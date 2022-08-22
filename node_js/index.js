a66113dcd9574c74f991785dade1331808a970f7
vasiliy <games480@gmail.com>
2022-08-23 00:36:47 +0300

    Lesson 6. Form processing

--- node_js/index.js
+++ node_js/index.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.get('/', (req, res) => {
-    // res.end('Hello from nodejs');
     res.render('index')
+});
+
+app.post('/', (req, res) => {
+
+    const { city } = req.body;
+
+    console.log(city);
+    res.render('index');
 });
 
 app.listen(3000, () => {
